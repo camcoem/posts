84c14813d7ad88f06469d55cb271cbdabfeec09f
Camila Coello Merino <camila.c.merino@gmail.com>
2024-03-16 14:15:18 +0100

    add: colors and fontFamily to tailwind.config.ts
    Took 4 minutes

--- app-client/tailwind.config.ts
+++ app-client/tailwind.config.ts
@@ -1,17 +1,45 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        light: ['Inter', 'sans-serif'],
+        serif: ['Inter', 'sans-serif'],
+        bold: ['inter', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        display: ['Inter', 'sans-serif'],
+        mono: ['monospaced'],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#1f1235',
+        },
+        main: {
+          DEFAULT: '#FFFFFF',
+        },
+        secondary: {
+          DEFAULT: '#67568c',
+        },
+        tertiary: {
+          DEFAULT: '#ff6e6c',
+        },
+        highlight: {
+          DEFAULT: '#fbdd74',
+        },
+        cardBackground: {
+          DEFAULT: '#e2daeb',
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
