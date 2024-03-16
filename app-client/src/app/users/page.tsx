14a307cab0049f0212e0c0df0665f76e8d2f1c1a
Camila Coello Merino <camila.c.merino@gmail.com>
2024-03-16 15:52:49 +0100

    add: fetching users from users page.tsx, as well as minimal layout for displaying names. Still commented out Link component until creating the following needed routes.
    Took 3 minutes

--- app-client/src/app/users/page.tsx
+++ app-client/src/app/users/page.tsx
@@ -1,5 +1,34 @@
-export default function Users() {
+import { IUser } from '@/types/types';
+import Link from 'next/link';
+
+const getUsers = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    if (!res.ok) {
+      throw new Error('Bad Response!');
+    }
+
+    return await res.json();
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default async function Users() {
+  const users: IUser[] = await getUsers();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-main p-24"></main>
+    <div className=" h-screen bg-main">
+      <ul className="flex flex-col gap-3">
+        {users.map((user) => (
+          // <Link href={}>
+          <li key={user.id} className="text-primary">
+            {user.name ?? ''}
+          </li>
+          // </Link>
+        ))}
+      </ul>
+    </div>
   );
 }
