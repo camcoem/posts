af4071b0abe736e06077ea6aee6c81210383812f
Camila Coello Merino <camila.c.merino@gmail.com>
2024-03-16 15:38:32 +0100

    add: fetching posts with respective error handling, try and catch for error and !res.ok for a bad response from api.
    add: IPost interface
    
    add: basic layout for mapping posts and displaying titles
    
    --> Had some TS Errors to fix, so it took a bit longer than expected
    
    Took 58 minutes

--- app-client/src/app/posts/page.tsx
+++ app-client/src/app/posts/page.tsx
@@ -1,5 +1,36 @@
-export default function Posts() {
+const getPosts = async (): Promise<IPost[]> => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    if (!res.ok) {
+      throw new Error('Bad Response!');
+    }
+
+    return await res.json();
+  } catch (err) {
+    throw err;
+  }
+};
+
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export default async function Posts() {
+  const posts: IPost[] = await getPosts();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-main p-24"></main>
+    <div className=" bg-main">
+      <ul className="flex flex-col gap-3">
+        {posts.map((post) => (
+          <li key={post.id} className="text-primary">
+            {post.title ?? ''}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
