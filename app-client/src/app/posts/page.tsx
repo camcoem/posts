e8bb1bd23ceb361149271d8f79b2232da2da8c28
Camila Coello Merino <camila.c.merino@gmail.com>
2024-03-16 17:27:04 +0100

    add: styling to posts in page.tsx.
    --> I know DRY wished I could have abstracted the component, had no time :(
    
    Took 2 minutes

--- app-client/src/app/posts/page.tsx
+++ app-client/src/app/posts/page.tsx
@@ -18,11 +18,16 @@ export default async function Posts() {
   const posts: IPost[] = await getPosts();
 
   return (
-    <div className=" bg-main">
+    <div className=" bg-main p-6">
       <ul className="flex flex-col gap-3">
         {posts.map((post) => (
-          <li key={post.id} className="text-primary">
-            {post.title ?? ''}
+          <li key={post.id} className="flex w-full flex-col items-start text-primary">
+            <div className=" w-full rounded-full border border-secondary p-4 shadow-xl hover:bg-secondary/30">
+              <div className=" ml-4 text-xl text-tertiary hover:text-tertiary/50">
+                {post.title ?? ''}
+              </div>
+              <div className="ml-4 text-sm text-primary">{post.body ?? ''}</div>
+            </div>
           </li>
         ))}
       </ul>
