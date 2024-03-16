5cb4636c1a8824a2face902e826d22ac4b1f10db
Camila Coello Merino <camila.c.merino@gmail.com>
2024-03-16 16:36:15 +0100

    add: getUser fetching user with id, using id from params. With just a little layout to check in data is being fetched properly
    Took 19 minutes

--- app-client/src/app/users/[id]/page.tsx
+++ app-client/src/app/users/[id]/page.tsx
@@ -1,7 +1,26 @@
-export default function UserWithId({ params }: { params: { userId: string } }) {
+import { IUser } from '@/types/types';
+
+const getUser = async (id: string) => {
+  try {
+    const userRes = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    if (!userRes.ok) {
+      throw new Error('Bad Response!');
+    }
+
+    return await userRes.json();
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default async function UserWithId({ params }: { params: { id: string } }) {
+  const user: IUser = await getUser(params.id);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-main p-24">
-      <h1 className="text-primary">HOLA</h1>
+    <main className="flex min-h-screen flex-col items-start bg-main p-24">
+      <h1 className="text-primary">{user.name}</h1>
+      <h1 className="text-primary">Hola {user.name}</h1>
     </main>
   );
 }
