8ec6468ba517a7336bb3e6d7bece3df9a9fe22b9
Camila Coello Merino <camila.c.merino@gmail.com>
2024-03-22 10:49:39 +0100

    add: responsiveness for PostCard.tsx component
    Took 3 minutes

--- app-client/src/components/Post/PostCard.tsx
+++ app-client/src/components/Post/PostCard.tsx
@@ -5,7 +5,7 @@ export default function PostCard(props: IPost) {
 
   return (
     <li key={id} className="flex w-full flex-col items-start text-primary">
-      <div className=" w-full rounded-full border border-secondary p-4 shadow-xl hover:bg-secondary/30">
+      <div className=" w-full rounded-3xl border border-secondary p-4 shadow-xl hover:bg-secondary/30 lg:rounded-full">
         <div className=" ml-4 text-xl text-tertiary hover:text-tertiary/50">{title ?? ''}</div>
         <div className="ml-4 text-sm text-primary">{body ?? ''}</div>
       </div>
